d785728c743018c0b8847a8f71a1aa20fe60c9bd
ribeiroAllison <allisonribeiro@gmail.com>
2023-02-17 13:24:15 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Route path="/:type?">
+        <HomePage />
+      </Route>
+
+
+    </Router>
+  );
+}
+
+export default App;
